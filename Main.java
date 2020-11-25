763557f98c9a15fbaf0b545079eae29cfe4f4c86
alsousha <47866010+alsousha@users.noreply.github.com>
2020-11-25 15:47:31 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        double ticket = 80.0;
+        int mile;
+        int bonus;
+        mile = (int)ticket/bonus;
+        System.out.println(mile);
+
+    }
+}
